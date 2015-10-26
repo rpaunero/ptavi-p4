0afcbadd729f2a4fbbe5125e7ca8fe24ef96d370
rpaunero <r.paunero@alumnos.urjc.es>
2015-10-26 17:43:26 +0100

    EXPIRES (Iniciando apartado 7)

--- client.py
+++ client.py
@@ -13,9 +13,8 @@ import sys
 # Dirección IP del servidor.
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
-# Contenido que vamos a enviar
-#Line_List = sys.argv[3:]
-#LINE = ' '.join(Line_List)
+DIRECCION = sys.argv[4]
+TIME = sys.argv[5]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -23,10 +22,11 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 if sys.argv[3] == 'register':
+    TIEMPO = ("Expires: " + TIME + '\r\n\r\n')
     REGISTER  = ('REGISTER' + ' ' + 'sip:' + sys.argv[4] + ' ' + 'SIP/2.0')
-    #print("Enviando: " + LINE)
-    my_socket.send(bytes(REGISTER, 'utf-8') + b'\r\n\r\n')
+    my_socket.send(bytes(REGISTER , 'utf-8') + b'\r\n\r\n' + bytes(TIEMPO, 'utf-8'))
     data = my_socket.recv(1024)
+    
 
 print('Recibido -- ', data.decode('utf-8'))
 print("Terminando socket...")
