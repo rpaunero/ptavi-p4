27c6bdc66d1b72d91bb6165e0a607ac54d0294b9
rpaunero <r.paunero@alumnos.urjc.es>
2015-11-02 19:04:19 +0100

    Pep8

--- server.py
+++ server.py
@@ -12,13 +12,14 @@ import time
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
-    Echo server class
+    Clase: manejador de Register en SIP
     """
-    
     dicc = {}
 
-            
     def register2json(self):
+        """
+        Leo datos registrados en un diccionario
+        """
         try:
             with open('registered.json', 'w') as ff:
                 json.dump(self.dicc, ff)
@@ -26,37 +27,40 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             pass
 
     def json2register(self):
+        """
+        Leo datos registrados de un fichero json
+        """
         try:
             with open('registered.json', 'r') as ff:
                 self.dicc = json.load(ff)
         except:
             pass
 
-
     def handle(self):
-        #print(self.client_address)
+        """
+        Identifica y procesa las peticiones del cliente
+        """
         IP = self.client_address[0]
         print('IP' + ':' + IP)
         PORT = self.client_address[1]
         print('PORT' + ':' + str(PORT))
-        
+
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.json2register()
-        
+
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             if not line:
                 break
-            
+
             line = line.decode('utf-8')
             print("El cliente nos manda " + line)
-           
-                     
-            elemento = line.split(' ')            
+
+            elemento = line.split(' ')
             if elemento[0] == 'REGISTER':
                 if not '@' in elemento[1].split(':')[1]:
-                   break
+                    break
 
                 formato = '%Y-%m-%d %H:%M:%S'
                 direccion = elemento[1].split(':')[1]
@@ -69,25 +73,21 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 print('Expires traza:' + time_expires)
                 print(time_expires + ('....') + current_time)
 
-
                 temp_list = []
                 for usuario in self.dicc:
-                   
-                   if (time.strptime(self.dicc[usuario][1], formato) <= current_time1):
-                       temp_list.append(usuario) 
-                       print('Añado a : ' + usuario + 'lista temporal')
-                            #del self.dicc[direccion]
-                            #print('eliminamos:' + direccion)
+
+                    if (time.strptime(self.dicc[usuario][1], formato) <= current_time1):
+                        temp_list.append(usuario)
+                        print('Añado a : ' + usuario + 'lista temporal')
                 for usuario in temp_list:
                     del self.dicc[usuario]
                     print('eliminamos:' + usuario)
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                 self.register2json()
 
-            # Si no hay más líneas salimos del bucle infinito
 
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos 
-    serv = socketserver.UDPServer(('',int(sys.argv[1])), SIPRegisterHandler)
+    # Creamos servidor de eco y escuchamos
+    serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
